2340bd2f41d8842ac1aaf32374eeeb35285c1781
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2024-02-09 17:58:57 -0300

    Added database layer

---
+++ Infraestructure.Database/Security/Tables/Roles.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Security].[Roles] (
+    [Id]          INT           NOT NULL,
+    [Name]        NVARCHAR (50) NOT NULL,
+    [Description] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
